c7e6d04363b020376fa686ddaba1005247057c9e
ameyakulkarni <abkulkar@usc.edu>
2019-12-27 15:11:31 -0800

    created choice component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChoiceComponent } from './choice/choice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChoiceComponent
   ],
   imports: [
     BrowserModule,
