89adbcf595262c7fac726669c17cf8fff992ddf9
ameyakulkarni <abkulkar@usc.edu>
2019-12-27 15:12:32 -0800

    added teamlist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChoiceComponent } from './choice/choice.component';
+import { TeamListComponent } from './team-list/team-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChoiceComponent
+    ChoiceComponent,
+    TeamListComponent
   ],
   imports: [
     BrowserModule,
